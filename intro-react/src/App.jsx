7bd9452ce894495ce251860c12b14baeca22cd60
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-10-20 18:06:40 -0300

    add props to `Card` component

--- intro-react/src/App.jsx
+++ intro-react/src/App.jsx
@@ -3,7 +3,15 @@ import { Card } from './Card'
 export function App() {
   return (
     <>
-      <Card />
+      <Card 
+        title="The Boys" 
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita iure eligendi ipsa facilis provident id voluptas ipsam, labore sit illum. Nobis ad reiciendis veritatis itaque doloribus id ipsa natus laboriosam!" 
+      />
+
+      <Card 
+        title="Breaking Bad" 
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita iure eligendi ipsa facilis provident id voluptas ipsam, labore sit illum. Nobis ad reiciendis veritatis itaque doloribus id ipsa natus laboriosam!" 
+      />
     </>
   )
 }
--- intro-react/src/Card.jsx
+++ intro-react/src/Card.jsx
@@ -1,3 +1,10 @@
-export function Card() {
-    return <h1>Card</h1>
+export function Card(props) {
+    const { title, description } = props
+
+    return (
+        <>
+            <h1>Title: { title }</h1>
+            <p>Description: { description }</p>
+        </>
+    )
 }
