fe115020180a1ddce3e571d03617ac82ff18330b
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-10-20 19:26:19 -0300

    add style to `App` component

--- intro-react/src/App.jsx
+++ intro-react/src/App.jsx
@@ -1,8 +1,11 @@
 import { Card } from './Card'
+import { Header } from './components/Header'
+import './style.css'
 
 export function App() {
   return (
     <>
+      <Header />
       <Card 
         title="The Boys" 
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita iure eligendi ipsa facilis provident id voluptas ipsam, labore sit illum. Nobis ad reiciendis veritatis itaque doloribus id ipsa natus laboriosam!" 
