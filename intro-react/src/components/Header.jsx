dfb5f1b7508b2a5bcd694b2cbdac5afc326cd83d
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-10-20 19:25:03 -0300

    add `Header` component

---
+++ intro-react/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import style from './Header.module.css'
+
+export function Header() {
+    return (
+        <header className={style.header}>
+            <h1>Header</h1>
+        </header>
+    )
+}
