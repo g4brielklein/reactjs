066381d1a08f3e7835cfe26777d1954b4d615998
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-10-21 23:02:33 -0300

    add header logo and css adjusts

--- intro-react/src/components/Header.jsx
+++ intro-react/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import style from './Header.module.css'
+import logo from '../assets/logo.svg'
 
 export function Header() {
     return (
-        <header className={style.header}>
-            <h1>Header</h1>
+        <header className={ style.header }>
+            <img src={ logo } alt="" />
+            <h1>Series Crud</h1>
         </header>
     )
 }
