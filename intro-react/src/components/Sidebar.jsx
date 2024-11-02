0b70c7cf1766953669dd20aa1d92b455fadaabc9
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-11-02 18:45:27 -0300

    adjust mocked data and cover/profile pic size and style

--- intro-react/src/components/Sidebar.jsx
+++ intro-react/src/components/Sidebar.jsx
@@ -1,26 +1,30 @@
 import style from './Sidebar.module.css'
 
-import userProfileCover from '../assets/user-profile-cover.svg'
-import userProfileImg from '../assets/user-profile-img.svg'
 import editIcon from '../assets/edit-icon.svg'
 
 export function Sidebar() {
+    // mocked data
     const user = {
         name: 'Gabriel Klein',
-        role: 'Software Developer'
+        role: 'Software Developer',
+        profileCoverImg: 'https://images.unsplash.com/photo-1508830524289-0adcbe822b40?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        profilePicImg: 'https://github.com/g4brielklein.png',
     }
 
     return (
         <aside className={ style.sidebar }>
             <div className={ style.coverContainer }>
-                <img src={ userProfileCover } alt="User image cover" />
+                <img 
+                    src={ user.profileCoverImg } 
+                    alt="User image cover" 
+                />
             </div>
             <div className={ style.mainContainer }>
                 <div className={ style.profilePictureContainer }>
-                    <img src={ userProfileImg } alt="User profile image" />
+                    <img src={ user.profilePicImg } alt="User profile image" />
                 </div>
-                <p className={ style.userName }>{ user.name }</p>
-                <p className={ style.userRole }>{ user.role }</p>
+                <strong className={ style.userName }>{ user.name }</strong>
+                <span className={ style.userRole }>{ user.role }</span>
             </div>
             <div className={ style.bottomContainer }>
                 <div className={ style.editButton }>
