6068df7d46429b2f73c9d0537d2884864b4f395e
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-12-31 15:27:50 -0300

    add `InternalServerError` custom error

--- techhub-api/errors.js
+++ techhub-api/errors.js
@@ -1,10 +1,26 @@
 export class ResourceNotFoundError extends Error {
-    constructor(props) {
-        super(props.message || 'Resource not found');
+    constructor(message, props) {
+        super(message || 'Resource not found');
 
         this.name = "ResourceNotFoundError"
-        this.message = props.message || "The wanted resource was not found"
-        this.statusCode = props.statusCode || 404;
+        this.message = message || "The wanted resource was not found"
+        this.statusCode = props?.statusCode || 404;
+    }
+
+    toJSON() {
+        return {
+            name: this.name,
+            status_code: this.statusCode,
+        }
+    }
+}
+
+export class InternalServerError extends Error {
+    constructor(message, props) {
+        super(message || "Internal Server Error");
+
+        this.name = "InternalServerError";
+        this.statusCode = props?.statusCode || 500;
     }
 
     toJSON() {
