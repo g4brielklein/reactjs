6fc3748285e136c90dcad9ced6322b16d90d2990
Gabriel Klein <gabrielkleinribas@gmail.com>
2025-01-04 17:26:17 -0300

    remove mocked data

--- techhub/src/App.jsx
+++ techhub/src/App.jsx
@@ -26,41 +26,6 @@ export function App() {
     }
   }, [data])
 
-  // mocked data
-  // const posts = [
-  //   {
-  //     user: {
-  //       name: 'Gabriel Klein',
-  //       role: 'Software Developer',
-  //       profileImageUrl: 'https://github.com/g4brielklein.png',
-  //     },
-  //     content: `Hey everybody, I want to let you guys know that I've just started at a new position as a Software Developer at TechHub! 🚀 #softwareEngineering #development`,
-  //     publishTime: new Date('2024-11-07 16:50:25'),
-  //     comments: [
-  //       {
-  //         user: {
-  //           name: 'Gabriel Klein',
-  //           role: 'Software Developer',
-  //           profileImageUrl: 'https://github.com/g4brielklein.png',
-  //         },
-  //         content: 'Congrats, man! 👏',
-  //         likes: 3,
-  //         publishTime: new Date('2024-11-14 18:53:43'),
-  //       },
-  //       {
-  //         user: {
-  //           name: 'Kyle Simpson',
-  //           role: 'Software Developer',
-  //           profileImageUrl: 'https://github.com/getify.png',
-  //         },
-  //         content: 'Awesome!',
-  //         likes: 32,
-  //         publishTime: new Date('2024-11-17 13:53:43'),
-  //       }
-  //     ]
-  //   }
-  // ]
-
   return (
     <div>
       <Header />
